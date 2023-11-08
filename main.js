517643cfdc0719d87b389f098735cd4c698f5f68
kadriumar <kadriumar5@gmail.com>
2023-11-08 16:49:25 +0530

    Adding multiple users

--- main.js
+++ main.js
@@ -3,10 +3,12 @@ var form = document.getElementById("my-form");
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  var formData = {
+  var user = {
     name: document.getElementById("name").value,
     email: document.getElementById("email").value,
   };
 
-  localStorage.setItem("formData", JSON.stringify(formData));
+  var users = JSON.parse(localStorage.getItem("users")) || [];
+  users.push(user);
+  localStorage.setItem("users", JSON.stringify(users));
 });
